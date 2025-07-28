9015191a81085649cab8df033666f67b238faa67
Zachary Lincoln (personal) <zachlincoln@macmini.lan>
2025-07-28 10:47:51 -0400

    added cloudflare astro package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import flowbiteReact from "flowbite-react/plugin/astro";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), flowbiteReact()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
