c6ed87baa4ec20b56eb0f179758b509b2a8e68ff
Zachary Lincoln (personal) <zachlincoln@macmini.lan>
2025-08-08 03:27:29 -0400

    built desktop nav

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+import flowbite from 'flowbite/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    flowbite,
+  ],
+}
