f42e7c7a3fb424fb0c903cce8b8d8152929f1df9
Zachary Lincoln (personal) <zachlincoln@macmini.lan>
2025-08-08 05:51:41 -0400

    big package changes to support addition of contact modal.  theme toggle was added into the nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
+// @ts-check
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import flowbiteReact from "flowbite-react/plugin/astro";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), flowbiteReact()],
-
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind()],
 
   adapter: cloudflare(),
 });
